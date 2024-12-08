7aa3672f44c6dd1c7b8d5588012e753448e426c2
Avalon Parton <avalonlee@gmail.com>
2024-12-07 17:37:12 -0800

    update to 1.21.4

--- scripts/download-fabric.sh
+++ scripts/download-fabric.sh
@@ -7,7 +7,7 @@ set -o pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Download fabric. URL from https://fabricmc.net/use/server/
-URL="https://meta.fabricmc.net/v2/versions/loader/1.21.3/0.16.9/1.0.1/server/jar"
+URL="https://meta.fabricmc.net/v2/versions/loader/1.21.4/0.16.9/1.0.1/server/jar"
 JAR="$(realpath ${SCRIPT_DIR}/../server/fabric.jar)"
 
 wget "${URL}" --output-document="${JAR}"
